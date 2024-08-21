400f133001c1398f1340c3e7da2be2a185a56277
Jes <141727697+octyn-yt@users.noreply.github.com>
2024-08-21 01:29:51 +0100

    Delete enter.js

--- enter.js
+++
@@ -1,6 +0,0 @@
-function enter() {
-    document.getElementById("enter").classList.add("hidden");
-    document.getElementById("content").classList.remove("hidden");
-    var audio = document.getElementById("bgm");
-    audio.play();
-}
